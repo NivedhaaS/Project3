1e3573e6114af574b7b5796ada1a6eedaa27f598
lynettehemingway <147886775+lynettehemingway@users.noreply.github.com>
2024-11-28 18:23:03 -0500

    frontend updated

---
+++ uweather/src/components/Navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './navbar.css';
+import earth from "../../assets/earth.png";
+
+const Navbar = () => {
+    return (
+        <div>y is this not printing sigh.</div>
+        // <nav className="navbar">
+        //     <img src={earth} alt="earth" className='earth'/>
+
+        //     <button className="github">
+        //         <img src="" alt="" className="githubIcon"/>
+        //     </button>
+        // </nav>
+    )
+}
+
+export default Navbar;
