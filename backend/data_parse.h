14db4edb3ce475d2ffdac63a43cf05c5a2dab126
NivedhaaS <148982596+NivedhaaS@users.noreply.github.com>
2024-11-29 14:57:35 -0500

    updated backend

--- backend/data_parse.h
+++ backend/data_parse.h
@@ -1,3 +1,13 @@
 #include <filesystem>
-#include <ifstream>
+#include <fstream>
+#include <string>
+#include <vector>
 //do not delete
+
+struct dataEntry{
+    std::string name;
+    std::double latitude;
+    std::double longitude;
+};
+
+std::vector<DataEntry> loadData(std::filesystem::path& file_path);
