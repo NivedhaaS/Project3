912757e47c1565ee26686ba9de75573bbd774b88
lynettehemingway <147886775+lynettehemingway@users.noreply.github.com>
2024-11-29 22:28:07 -0500

    updated

---
+++ uweather/src/components/Navbar/Description/description.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Description = () => {
+    return (
+        <nav className="description">
+            <img src={logo} alt="logo" className="logo"/> 
+        </nav>
+    )
+}
+
+export default Description;
