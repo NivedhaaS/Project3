7c9cae3ca94ff0bb6fa1a12ec443c246fc5e1db1
lynettehemingway <147886775+lynettehemingway@users.noreply.github.com>
2024-11-30 00:13:52 -0500

    updated

--- uweather/src/components/Hotspots/hotspots.js
+++ uweather/src/components/Hotspots/hotspots.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import sun from "../../assets/sun.png";
+import map from "../../assets/usa.png";
 import "./hotspots.css";
 
 const Hotspots = () => {
     return (
         <section id="hotspots">
-            <img src={sun} alt="sun" className="sun"/>
-            <div className="hHotspots" >
-                <span>global warming hotspots -------------------------------</span>
+            <div class="container">
+                <img src={sun} alt="sun" className="sun"/>
+                <div className="hHotspots" >
+                    <span>global warming hotspots -------------------------------</span>
+                </div>
             </div>
-        </section>                                                                                          
-    );
+            <img src={map} alt="map" className="map"/>
+        </section> 
+    )
 }
 
 export default Hotspots;
