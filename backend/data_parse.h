ff7fb3b8f13616ff8b17c22944a708a838ecb893
NivedhaaS <148982596+NivedhaaS@users.noreply.github.com>
2024-12-02 17:44:58 -0500

    updated regression calculation

--- backend/data_parse.h
+++ backend/data_parse.h
@@ -14,7 +14,6 @@ struct dataEntry{
 };
 
 
-
 struct location : public dataEntry{
     double percentage_increased;
 
@@ -34,12 +33,10 @@ struct location : public dataEntry{
 };
 
 
-
 inline void trim(std::string& trim_str){
     trim_str.erase(trim_str.find_last_not_of(' ') + 1);
     trim_str.erase(0, trim_str.find_first_not_of(' '));
 }
-
 
 
 std::unordered_map<std::string, dataEntry> loadStations(std::string data_path = "../data"){
@@ -72,7 +69,6 @@ std::unordered_map<std::string, dataEntry> loadStations(std::string data_path = 
 };
 
 
-
 void loadData(std::string data_path = "../data", const std::unordered_map<std::string, dataEntry>& station_codes, std::priority_queue<location>& heap, std::unordered_map<std::string, location>& table){
     data_path += "/rawtest";
 
@@ -92,13 +88,37 @@ void loadData(std::string data_path = "../data", const std::unordered_map<std::s
             while (std::getline(station_file, data_line)){
                 if (data_line.substr(17, 4) == "TMAX"){
                     //to-do: implement math to calculate regression of temp vs year
+                    int year = std::stoi(data_line.substr(0, 4));  // Extract year (first 4 digits)
+                    int temperature = std::stoi(data_line.substr(6, 5));  // Extract temperature (next 5 digits)
 
-
+                    years.push_back(year);
+                    temperatures.push_back(temperature);
                 }
                 else continue;
+                if (years.size() > 1){
+                    int N = years.size();
+                    int sum_x = 0; 
+                    int sum_y = 0; 
+                    int sum_xy = 0;
+                    int sum_x2 = 0;
+
+                    for (int i = 0; i < N; i++){
+                        sum_x += years[i];
+                        sum_y += temperatures[i];
+                        sum_xy += years[i] * temperatures[i];
+                        sum_x2 += years[i] * years[i];
+                    }
+
+                    //To calculate slope and intercept
+                    double slope = (N * sum_xy - sum_x * sum_y) / double(N * sum_x2 - sum_x * sum_x);
+                    double intercept = (sum_y - m * sum_x) / double(N);
+
+                    station_location.percentage_increased = slope * 100;
+
+                    table[file.path().stem().string()] = station_location;
+                    heap.push(station_location);
+                }
             }
-
-
             station_file.close();
         }
 
