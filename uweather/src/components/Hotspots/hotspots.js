aa3ec1e300f314e768bbd397fdbdfe9a7dfb385e
lynettehemingway <147886775+lynettehemingway@users.noreply.github.com>
2024-12-02 21:50:34 -0500

    updated

--- uweather/src/components/Hotspots/hotspots.js
+++ uweather/src/components/Hotspots/hotspots.js
@@ -1,37 +1,25 @@
-import React from "react";
-import sun from "../../assets/sun.png"; // Replace with your actual path
-import map from "../../assets/usa.png"; // Replace with your actual path
+import React from 'react';
+import sun from "../../assets/sun.png";
+import map from "../../assets/usa.png";
 import "./hotspots.css";
 
 const Hotspots = () => {
-  return (
-    <section id="hotspots">
-      <div className="container">
-        <div className="hotspot-header">
-          <img src={sun} alt="Sun" className="sun" />
-          <span>
-            Global Warming Hotspots
-            <hr className="divider" />
-          </span>
-        </div>
-        <img src={map} alt="Map" className="map" />
-        <div className="controls">
-          <label htmlFor="hotspot-count">How many hotspots to find:</label>
-          <input
-            type="number"
-            id="hotspot-count"
-            name="hotspot-count"
-            placeholder="##"
-            className="input-box"
-          />
-          <div className="buttons">
-            <button className="button">Heap</button>
-            <button className="button">Table</button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+    return (
+        <section id="hotspots">
+            <div class="container">
+                <img src={sun} alt="sun" className="sun"/>
+                <div className="hHotspots" >
+                    <span>global warming hotspots<hr className="hr"></hr></span>
+                    <div className="find">how many hotspots to find: 
+                    <input type="number" id="name" name="name" placeholder=" ##"/>
+                    <button className="button">find hotspots</button>
+                    <br/>
+                    </div>
+                </div>
+            </div>
+            <img src={map} alt="map" className="map"/>
+        </section> 
+    )
+}
 
 export default Hotspots;
