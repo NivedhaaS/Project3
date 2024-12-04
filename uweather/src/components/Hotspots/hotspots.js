0bc266dfe820a8843af63c516352306e2d960599
lynettehemingway <147886775+lynettehemingway@users.noreply.github.com>
2024-12-04 10:35:41 -0500

    yi updated

--- uweather/src/components/Hotspots/hotspots.js
+++ uweather/src/components/Hotspots/hotspots.js
@@ -2,48 +2,63 @@ import React, { useState } from "react";
 import sun from "../../assets/sun.png";
 import map from "../../assets/usa.png";
 import "./hotspots.css";
-import hotspotData from "./test.json";
 
-function calculateY(lat){
-    function webMercatorY(latitude){
-        const radLat = (latitude * Math.PI) / 180;
-        return Math.log(Math.tan(Math.PI / 4 + radLat / 2));
-    }
+function calculateY(lat) {
+  function webMercatorY(latitude) {
+    const radLat = (latitude * Math.PI) / 180;
+    return Math.log(Math.tan(Math.PI / 4 + radLat / 2));
+  }
 
-    const yMin = webMercatorY(22);
-    const yMax = webMercatorY(50);
+  const yMin = webMercatorY(22);
+  const yMax = webMercatorY(50);
 
-    const yMercator = webMercatorY(lat);
+  const yMercator = webMercatorY(lat);
 
-    const yNormalized = (yMercator - yMin) / (yMax - yMin);
-    const yPercentage = (1 - yNormalized) * 100;
-    console.log(yPercentage);
-    return yPercentage;
+  const yNormalized = (yMercator - yMin) / (yMax - yMin);
+  const yPercentage = (1 - yNormalized) * 100;
+  console.log(yPercentage);
+  return yPercentage;
+}
+
+function usingStructure(using_table){
+  if (using_table) return "table";
+  else return "heap";
 }
 
 const Hotspots = () => {
-  const [hotspots, setHotspots] = useState(""); 
-  const [plottedHotspots, setPlottedHotspots] = useState([]); 
+  const [hotspots, setHotspots] = useState("");
+  const [plottedHotspots, setPlottedHotspots] = useState([]);
+  const [using_table, set_table] = useState(false);
 
   const findHotspots = () => {
-    const numHotspots = parseInt(hotspots, 10); 
+    const numHotspots = parseInt(hotspots, 10);
+    const structure = usingStructure(using_table);
     if (isNaN(numHotspots) || numHotspots <= 0) {
       alert("please enter a valid number of hotspots.");
       return;
     }
 
+    try{
+    const response = fetch(`/api?hotspots=${numHotspots}&data_structure=${structure}`,{
+      method: 'GET'
+    })
+
+    const hotspotData = response.json();
+
     const formattedHotspots = hotspotData.data.map((item) => ({
-      lat: parseFloat(item.latitude), 
-      lng: parseFloat(item.longitude), 
+      lat: parseFloat(item.latitude),
+      lng: parseFloat(item.longitude),
       name: item.name,
       rank: item.rank,
       state: item.state,
       tempIncrease: item["temp_incrs/yr"],
-      duration: item["duration"], 
+      duration: item["duration"],
     }));
 
     const selectedHotspots = formattedHotspots.slice(0, numHotspots);
     setPlottedHotspots(selectedHotspots);
+  }
+  catch{}
   };
 
   return (
@@ -57,24 +72,29 @@ const Hotspots = () => {
           </span>
         </div>
       </div>
-
       <div className="map-container">
-        <img src={map} alt="map" className="map" />
-        {plottedHotspots.map((hotspot, index) => (
-          <div
-            key={index}
-            className="hotspot-marker"
-            style={{
-            //   top: `${((50 - hotspot.lng)/28) * 100}%`,
-              top: `${calculateY(hotspot.lng)}%`,
-              left: `${((130 + hotspot.lat)/65) * 100}%`, 
-            }}
-            title={`Name: ${hotspot.name}\nState: ${hotspot.state}\nRank: ${hotspot.rank}\nTemp Increase: ${hotspot.tempIncrease.toFixed(
-              2
-            )}/yr\nDuration: ${hotspot.duration}`}
-          ></div>
-        ))}
-      </div>
+  <img src={map} alt="map" className="map" />
+  {plottedHotspots.map((hotspot, index) => (
+    <React.Fragment key={index}>
+      <div
+        className="hotspot-marker"
+        style={{
+          top: `${calculateY(hotspot.lng)}%`,
+          left: `${((130 + hotspot.lat) / 65) * 100}%`,
+        }}
+        data-location={`
+        Lat: ${hotspot.lat}\n
+        Lng: ${hotspot.lng}\n
+        Name: ${hotspot.name}\n
+        State: ${hotspot.state}\n
+        Rank: ${hotspot.rank}\n
+        Temp Increase: ${hotspot.tempIncrease.toFixed(2)}/yr`}
+      >{hotspot.rank}</div>
+    </React.Fragment>
+  ))}
+</div>
+
+
 
       <div className="interaction_container">
         <label className="fj">
@@ -84,7 +104,7 @@ const Hotspots = () => {
             className="input_box"
             value={hotspots}
             onChange={(e) => setHotspots(e.target.value)}
-            placeholder="##"
+            placeholder="5"
           />
         </label>
         <button className="find" onClick={findHotspots}>
@@ -92,17 +112,20 @@ const Hotspots = () => {
         </button>
       </div>
 
-      <label class="togglecontainer">
-        <input type="checkbox" className="check"></input>
+      <label className="togglecontainer">
+        <input 
+          type="checkbox"
+          className="check"
+          checked={using_table}
+          onChange={(f) => set_table(f.target.checked)}></input>
         <span className="togglebackground">
           <div id="heapButton" className="toggletext">
             heap
           </div>
           <div id="tableButton" className="toggletext">
             table
-          </div>   
+          </div>
         </span>
-        
       </label>
       <div className="time-display"> </div>
     </section>
